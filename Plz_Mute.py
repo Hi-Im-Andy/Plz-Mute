ac4014d72f99f56cea602b4fb8c6c9d5e494ffaf
Andy Hernandez <eandyhe@gmail.com>
2022-05-10 17:01:40 -0700

    Added days, voice ouput, and strike counter
    There are 3 strikes per day so the day is updated every time. Also added text to speech to give a warning everytime a strike is given. Strikes are reset per day

--- Plz_Mute.py
+++ Plz_Mute.py
@@ -3,15 +3,40 @@ import os
 import pyaudio
 import audioop
 import math
+from datetime import date
+import playsound
+from gtts import gTTS
 
-p = pyaudio.pyaudio()
+def last_strike (today):
+    day_strike = date.today()
+    day_strike = day_strike.strftime("%m-%d-%Y")
+    
+    if (today == day_strike):
+        strikes -= 1
+        audio_string = "Frankie! This is a warning! Please Mute" # This is the audio that should be played
+        tts = gTTS(text = audio_string, lang="en")
+        audio_file = 'audio.mp3'
+        tts.save(audio_file)
+        playsound.playsound(audio_file)
+        os.remove(audio_file) #Deletes the audio file after it is played
+    else:
+        day_strike = today
+        strikes = 3
+    if(strikes <= 0):
+        return True
+    return False
+
+p = pyaudio.PyAudio()
 stream = p.open(format = pyaudio.paInt16,
  channels = 1, rate = 44100, input = True, 
  frames_per_buffer = 1024)
 
 while 1:
+    today = date.today()
+    today = today.strftime("%m-%d-%Y")
     data = stream.read(1024)
     rms = audioop.rms(data, 2)
-    decibel = 20 * math.log10(rms) #Converts rms to decibels
-    if(decibel > 85): #Checks if the sound is louder than 85 decibels
-        os.system("TASKKILL /F /IM application.exe") #Kills application
+    decibel = math.log10(rms) #Converts rms to decibels
+    if((decibel * 20)> 85): #Checks if the sound is louder than 85 decibels
+        if(last_strike(today)):
+            os.system("TASKKILL /F /IM application.exe") #Kills application
